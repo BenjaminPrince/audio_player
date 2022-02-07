4c5c9fa46829f1d292c5510b9bfa38093a336ee3
prince <prince-benjamin@hotmail.com>
2022-02-07 12:29:01 +0100

    modif

--- process/pdo.php
+++ process/pdo.php
@@ -2,7 +2,7 @@
 
 try {
 
-    $bdd = new PDO('mysql:host=localhost;dbname=player_audio;charset=utf8','root', '');
+    $bdd = new PDO('mysql:host=mysql;dbname=Audio;charset=utf8','root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e)
